905a92d397f0c0b109588565c311072abae8e6d7
Williams Parra Parra <williams.parra.parra@gmail.com>
2020-11-22 13:37:16 -0300

    Agregar vista listar medicamentos
    modificacion de urls de proyecto
    agregar vista html de listar medicamento
    agregar listar medicamentos a la navegacion

--- sistema_recetas_online/medicamentos/urls.py
+++ sistema_recetas_online/medicamentos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [    
-    path('', views.agregar_medicamento, name='agregar_medicamento'),
+    path('agregar/', views.agregar_medicamento, name='agregar_medicamento'),
+    path('listar/', views.listar_medicamentos, name='listar_medicamentos'),
 ]
--- sistema_recetas_online/medicamentos/views.py
+++ sistema_recetas_online/medicamentos/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def agregar_medicamento(request):
 
     return render(request, 'agregar_medicamento.html')
+
+def listar_medicamentos(request):
+
+    return render(request, 'listar_medicamentos.html')
